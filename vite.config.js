9ccd650742804fe9a90d0eec0f98111a8ee99f13
bhii06 <abhinayaaghnifadhila8g@gmail.com>
2026-07-04 14:20:15 +0700

    fitur: konfigurasi multi-page build berhasil

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: '/Instagram-tailwind/'
+  base: '/Instagram-tailwind/', // Sesuaikan kapital 'I' jika nama repo Anda memang besar
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        profile: resolve(__dirname, 'profile.html'),
+        highlight: resolve(__dirname, 'highlight.html'),
+      },
+    },
+  },
 })
